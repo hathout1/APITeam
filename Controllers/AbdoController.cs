503decc777e7a20593b1ddc6adc459b3260841ac
Abdelmonem Saad <abdelmonemsaad14@gmail.com>
2025-10-23 21:45:01 +0200

    Add AbdoController and setup .gitignore

---
+++ Controllers/AbdoController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace testTeamAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AbdoController : ControllerBase
+    {
+    }
+}
